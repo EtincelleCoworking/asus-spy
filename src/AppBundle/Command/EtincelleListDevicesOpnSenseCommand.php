165fa8333fc4ad13b6da3c84482ba0ac837fa597
Sebastien Hordeaux <shordeaux@waterproof.fr>
2018-06-01 14:56:50 +0200

    email check

--- src/AppBundle/Command/EtincelleListDevicesOpnSenseCommand.php
+++ src/AppBundle/Command/EtincelleListDevicesOpnSenseCommand.php
@@ -10,7 +10,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 use Goutte\Client;
 
-
 class EtincelleListDevicesOpnSenseCommand extends ContainerAwareCommand
 {
     protected function configure()
@@ -22,7 +21,6 @@ class EtincelleListDevicesOpnSenseCommand extends ContainerAwareCommand
             ->addOption('opnsense_api_secret', null, InputOption::VALUE_OPTIONAL, 'Password', '')
             ->addArgument('api', InputArgument::REQUIRED, 'API Endpoint to post detected devices');
     }
-
 
     protected function getDevicesFromOpnSense($host, $key, $secret, OutputInterface $output)
     {
@@ -54,11 +52,76 @@ class EtincelleListDevicesOpnSenseCommand extends ContainerAwareCommand
         return $devices;
     }
 
+    protected function getDevicesFromNmap(OutputInterface $output)
+    {
+        $range = null;
+
+        foreach ($this->getIPs() as $ip) {
+            if ($ip != '127.0.0.1') {
+                $ipParts = explode('.', $ip);
+                array_pop($ipParts);
+                $range = implode('.', $ipParts) . '.0/24';
+            }
+        }
+        if (!$range) {
+            $output->writeln('<error>Unable to find network IP</error>');
+            return false;
+        }
+        $result = array();
+        $output->writeln(sprintf('<info>Inspecting %s</info>', $range));
+        exec('nmap -sP ' . $range, $nmap);
+        print_r($nmap);
+        while (count($nmap) > 0) {
+            $line = array_shift($nmap);
+            if (preg_match('/Nmap scan report for (.+)( \(([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\))?/', $line, $tokens)) {
+                if (isset($tokens[2])) {
+                    $ip = $tokens[2];
+                    $name = $tokens[1];
+                } else {
+                    $ip = $tokens[1];
+                    $name = '';
+                }
+                $line = array_shift($nmap);
+                if (preg_match('/Host is up \(.+s latency\)\./', $line, $tokens)) {
+                    $line = array_shift($nmap);
+                    if (preg_match('/MAC Address: ([A-Z0-9]{2}:[A-Z0-9]{2}:[A-Z0-9]{2}:[A-Z0-9]{2}:[A-Z0-9]{2}:[A-Z0-9]{2}) \((.+)\)/', $line, $tokens)) {
+                        $mac = $tokens[1];
+                        $brand = $tokens[2];
+                        // var_dump(array($ip, $name, $mac, $brand));
+
+                        $result[$mac] = array(
+                            'name' => $name,
+                            'brand' => $brand,
+                            'mac' => $mac,
+                            'ip' => $ip,
+                            'lastSeen' => date('c'),
+                        );
+                    } else {
+                        $output->writeln(sprintf('<info>No match for %s</info>', $line));
+                    }
+                } else {
+                    $output->writeln(sprintf('<info>No match for %s</info>', $line));
+                }
+            } else {
+                $output->writeln(sprintf('<info>No match for %s</info>', $line));
+            }
+        }
+        return $result;
+    }
+
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $result = $this->getDevicesFromOpnSense($input->getOption('firewall_ip'), $input->getOption('opnsense_api_key'), $input->getOption('opnsense_api_secret'), $output);
-        print_r($result);
+        $result = $this->getDevicesFromNmap($output);
+        //print_r($result);
 
+        $opnsense_data = $this->getDevicesFromOpnSense($input->getOption('firewall_ip'), $input->getOption('opnsense_api_key'), $input->getOption('opnsense_api_secret'), $output);
+        foreach ($opnsense_data as $mac => $item) {
+            if (isset($result[$mac])) {
+                $result[$mac]['email'] = $item['email'];
+            }
+        }
+        //print_r($result);
         $output->writeln(sprintf('<comment>%d devices found</comment>', count($result)));
         $api = $input->getArgument('api');
         if (count($result) && $api) {
